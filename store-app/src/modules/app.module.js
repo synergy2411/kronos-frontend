45fe74278bb93443962f2350a9cdcf466ecb8181
unknown <Sumit Khandelwal>
2021-03-04 16:21:27 +0530

    store controller added

--- store-app/src/modules/app.module.js
+++ store-app/src/modules/app.module.js
@@ -1,4 +1,12 @@
 const app = angular.module("store-app", []);
 
+app.controller("StoreController", function($scope){
+    $scope.message = "Hello Angular";
+})
+
+app.controller("DemoController", function(){
+    this.message = "The Demo Controller";
+})
+
 
 export default app;
